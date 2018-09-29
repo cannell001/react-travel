01f318b5ced0787f1c2810a73ce23d05cc672d2b
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-29 10:31:37 -0400

    Coded Navbar.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Navbar from "./components/Nabar";
+import Navbar from "./components/Navbar";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <Nabar />
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <Navbar />
     </div>
   );
 }
