4c0e9477aad8771c4e2094736157e090014c7cde
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-29 11:54:25 -0400

    Reorganized Materilaize-css

--- src/index.js
+++ src/index.js
@@ -7,12 +7,10 @@ import "./styles.css";
 function App() {
   return (
     <div className="App">
-        <h1>Hello CodeSandbox</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        <Navbar />
+      <Navbar />
     </div>
   );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("home");
 ReactDOM.render(<App />, rootElement);
