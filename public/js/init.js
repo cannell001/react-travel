4410b8244f33cfec94177281f1f2dd1026b6732d
Jeremy Cannell <jcannell007@hotmail.com>
2018-09-29 15:16:28 -0400

    material box working

--- public/js/init.js
+++ public/js/init.js
@@ -4,6 +4,8 @@
 
     $(".scrollspy").scrollSpy();
 
+    $(".materialboxed").materialbox();
+
     $(".slider").slider({ full_width: true });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
